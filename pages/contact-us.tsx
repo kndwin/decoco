2ea8a3c8e33afed0bfe9e2a5e64e4a82d3fc725a
Kevin Nguyen <me@kndwin.dev>
2021-01-24 00:38:22 +1100

    🔨 fix contacts page to have maps and details on one side

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout/Layout'
 import utils from '../styles/utils.module.scss'
+import styles from './contact-us.module.scss'
 import ContactForm from '../components/ContactForm/ContactForm'
 import Map from '../components/Map/Map'
 
@@ -19,10 +20,32 @@ export default function ContactUs() {
       <h1 className={utils.title}>
         Visit Us
       </h1>
-      <div className={utils.width100}>
-        <p className={utils.description}>
-          Find us at 865 Punchbowl Road, Punchbowl
-        </p>
+      <div className={styles.mapDescription}>
+        <div className={styles.description}>
+          <h2 className={`${styles.titleH2}`}>
+            Location
+          </h2>
+          <p className={`${styles.fontDescription}`}>
+             865 Punchbowl Road, Punchbowl, NSW, 2196, Australia
+            <br/>
+          </p>
+          <h2 className={`${styles.titleH2}`}>
+           Phone number
+          </h2>
+          <p className={`${styles.fontDescription}`}>
+             0421606978
+          </p>
+          <h2 className={`${styles.titleH2}`}>
+             Opening hours
+          </h2>
+          <p className={`${styles.fontDescription}`}>
+            Monday - Friday: 9AM to 5PM
+            <br/>
+            Saturday: 10AM - 4PM
+            <br/>
+            Sunday: Closed
+          </p>
+        </div>
         <Map />
       </div>
     </Layout>
