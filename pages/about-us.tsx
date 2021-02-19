ca30ac41896176e2b0804cdd5722edfb2c1ec086
Kevin Nguyen <me@kndwin.dev>
2021-02-20 01:13:14 +1100

    🔨 changed the flex for contact-us, fixed typo for about-us and removed white background for BE01.png

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -8,9 +8,9 @@ export default function AboutUs() {
         About Us
       </h1>
       <p className={utils.description}>
-        We are Australian kitchen, furniture and vanity manufacturers based in Sydney, NSW.
-      </p>
-      <p className={utils.description}>
+        We are an Australian kitchen, furniture and vanity manufacturers based in Sydney, NSW.
+				<br />
+				<br />
         We provide high-quality, custom-made designs to suit your living space.
       </p>
     </Layout>
--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -6,7 +6,9 @@ import ContactForm from '../components/ContactForm/ContactForm'
 import Map from '../components/Map/Map'
 
 export default function ContactUs() {
+
   const [visible, setVisibility] = useState<boolean>(false)
+
   const handleFormVisbility = () => {
     setVisibility(!visible)
   }
@@ -16,44 +18,42 @@ export default function ContactUs() {
       <h1 className={utils.title}>
         Contact Us
       </h1>
-      <div className={styles.mapDescription}>
-        <div className={visible ? styles.form : styles.noForm}>
-          <h2 className={`${styles.titleH2}`}>
-            Fill out form below!
-          </h2>
-          <ContactForm isCancel={handleFormVisbility}/>
-        </div>
-        <div className={!visible ? styles.description : styles.noForm}>
-          <h2 className={`${styles.titleH2}`}>
-            Location
-          </h2>
-          <p className={`${styles.fontDescription}`}>
-             865 Punchbowl Road, Punchbowl, NSW, 2196, Australia
-            <br/>
-          </p>
-          <h2 className={`${styles.titleH2}`}>
-           Phone number
-          </h2>
-          <p className={`${styles.fontDescription}`}>
-            0401119989
-          </p>
-          <h2 className={`${styles.titleH2}`}>
-             Opening hours
-          </h2>
-          <p className={`${styles.fontDescription}`}>
-            Monday - Friday: 9AM to 5PM
-            <br/>
-            Saturday: 10AM - 4PM
-            <br/>
-            Sunday: Closed
-          </p>
-          <button className={styles.button}
-            onClick={handleFormVisbility}>
-            Get in touch
-          </button>
-        </div>
-        <Map />
-      </div>
+			<div className={visible ? styles.form : styles.noForm}>
+				<h2 className={`${styles.titleH2}`}>
+					Fill out form below!
+				</h2>
+				<ContactForm isCancel={handleFormVisbility}/>
+			</div>
+			<div className={!visible ? styles.description : styles.noForm}>
+				<h2 className={`${styles.titleH2}`}>
+					Location
+				</h2>
+				<p className={`${styles.fontDescription}`}>
+					 865 Punchbowl Road, Punchbowl, NSW, 2196, Australia
+					<br/>
+				</p>
+				<h2 className={`${styles.titleH2}`}>
+				 Phone number
+				</h2>
+				<p className={`${styles.fontDescription}`}>
+					0401119989
+				</p>
+				<h2 className={`${styles.titleH2}`}>
+					 Opening hours
+				</h2>
+				<p className={`${styles.fontDescription}`}>
+					Monday - Friday: 9AM to 5PM
+					<br/>
+					Saturday: 10AM - 4PM
+					<br/>
+					Sunday: Closed
+				</p>
+				<button className={styles.button}
+					onClick={handleFormVisbility}>
+					Get in touch
+				</button>
+			</div>
+			<Map />
     </Layout>
   )
 }
